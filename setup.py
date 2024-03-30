fd061a02939edf2dcbe0b05a51ba95853c55c19a
f-allum <fallum1@live.co.uk>
2024-03-30 10:44:04 -0700

    updated setup.py

--- setup.py
+++ setup.py
@@ -3,17 +3,14 @@ from setuptools import setup, find_packages
 VERSION = '0.0.1' 
 DESCRIPTION = 'PyCESim - classical simulation of Coulomb explosion'
 
-# Setting up
 setup(
-       # the name must match the folder name 'verysimplemodule'
         name="PyCESim", 
         version=VERSION,
         author="Felix Allum",
         author_email="<fallum@stanford.edu",
         description=DESCRIPTION,
         packages=find_packages(),
-        install_requires=['cclib'], # add any additional packages that 
-        # needs to be installed along with your package. Eg: 'caer'
-        
+        install_requires=['cclib, numpy, matplotlib, scipy, pandas'],
+        url='https://github.com/f-allum/PyCESim/archive/refs/tags/v0.0.1.tar.gz'
         keywords=['Coulomb explosion']
 )

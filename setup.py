ca5380f4cf2b3e0d006967fa21b649d67f3fd966
f-allum <fallum1@live.co.uk>
2024-03-30 11:00:52 -0700

    updated setup.py

--- setup.py
+++ setup.py
@@ -5,12 +5,14 @@ DESCRIPTION = 'PyCESim - classical simulation of Coulomb explosion'
 
 setup(
         name="PyCESim", 
+        packages = ['PyCESim'],
         version=VERSION,
         author="Felix Allum",
-        author_email="<fallum@stanford.edu",
+        author_email="fallum@stanford.edu",
         description=DESCRIPTION,
         packages=find_packages(),
         install_requires=['cclib', 'numpy', 'matplotlib', 'scipy', 'pandas'],
-        url='https://github.com/f-allum/PyCESim/archive/refs/tags/v0.0.1.tar.gz',
+        url='https://github.com/f-allum/PyCESim/',
+        download_url='https://github.com/f-allum/PyCESim/archive/refs/tags/v0.0.1.tar.gz',
         keywords=['Coulomb explosion']
 )
